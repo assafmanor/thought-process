cd9780e4e82b1a4e77543a54c14409e97ad4ce51
Assaf Manor <assaf44@gmail.com>
2019-12-16 19:39:15 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = 'foobar',
+    name = 'thoughtprocess',
     version = '0.1.0',
-    author = 'Dan Gittik',
-    description = 'An example package.',
+    author = 'Assaf Manor',
+    description = 'A mind reading and recording software.',
     packages = find_packages(),
-    install_requires = ['click', 'flask'],
+    install_requires = ['click', 'flask', 'Pillow'],
     tests_require = ['pytest', 'pytest-cov'],
 )
